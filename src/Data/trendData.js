20b73c0a3709a0f9ed3bf24253f4d40d83b93e53
rohit0985 <rohitsharma4681@gmail.com>
2023-02-20 02:52:45 +0530

    added dummy data

---
+++ src/Data/trendData.js
@@ -0,0 +1,26 @@
+export const trendData = [
+    {
+        name: "Minions",
+        shares: 97
+    },
+    {
+        name: "Avengers",
+        shares: 970
+    },
+    {
+        name: "Zainkeepscode",
+        shares: 78
+    },
+    {
+        name: "React.Js",
+        shares: 102
+    },
+    {
+        name: "ElonMusk",
+        shares: 150
+    },
+    {
+        name: "Redux",
+        shares: 95
+    },
+]
