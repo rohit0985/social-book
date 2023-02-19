4e763285811777e69da832a7fd2a247eb8017608
rohit0985 <rohitsharma4681@gmail.com>
2023-02-20 02:57:10 +0530

    added ui for the post

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Post.css";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
+import Share from "../../img/share.png";
+import Comment from "../../img/comment.png";
+
+const Post = ({ data }) => {
+  return (
+    <div className="post">
+      <img src={data.img} alt="image" />
+
+      <div className="postReactions">
+        <img src={data.liked ? Heart : NotLike} alt="" />
+        <img src={Comment} alt="" />
+        <img src={Share} alt="" />
+      </div>
+      <span>{data.likes} likes</span>
+      <div className="description">
+        <span>
+          <b>{data.name}</b>
+        </span>
+        <span> {data.desc}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
